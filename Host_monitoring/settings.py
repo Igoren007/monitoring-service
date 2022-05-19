7df3c25b1ff724bea82549177ae0e617b6009647
Igor Garmatenko <igoren92@gmail.com>
2022-05-19 12:41:36 +0300

    try celery

--- Host_monitoring/settings.py
+++ Host_monitoring/settings.py
@@ -128,3 +128,11 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'monitor.CustomUser'
+
+
+# REDIS related settings
+REDIS_HOST = 'localhost'
+REDIS_PORT = '6379'
+BROKER_URL = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
+BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 3600}
+CELERY_RESULT_BACKEND = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
